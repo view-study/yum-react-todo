6b0af521fffaa5384662e7ebc0f17a3f272cff02
= <=>
2022-11-27 16:46:57 +0900

    clock

--- src/screen/Clock/ClockContainer.js
+++ src/screen/Clock/ClockContainer.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import moment from "moment"
 import ClockPresenter from "./ClockPresenter";
 
 function ClockContainer() {
-  return <ClockPresenter />;
+  const [time, setTime] = useState("");
+  const [alarm, setAlarm] = useState(null);
+
+  useEffect(() => {
+    const timeInterver = setInterval(()=>{
+      const time = moment().format("HH:mm:ss");
+      setTime(time);
+    },100);
+
+    return () => {
+      clearInterval(timeInterver);
+    }
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(alarm)
+    }
+  }, [alarm]);
+
+  
+  const handleClickAlarm=(time) => () =>{
+
+    if(alarm) clearTimeout(alarm)
+
+    const alarmTimeout=setTimeout(()=>{
+      alert("예약한 알람입니다")
+    },time);
+
+    setAlarm(alarmTimeout)
+  }
+  
+
+  return <ClockPresenter time = {time} onClickAlarm={handleClickAlarm}/>;
 }
 
 export default ClockContainer;
--- src/screen/Clock/ClockPresenter.js
+++ src/screen/Clock/ClockPresenter.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function ClockPresenter() {
+function ClockPresenter({time,onClickAlarm}) {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', flexDirection: 'column' }}>
       <div style={{ height: 50, backgroundColor: '#cff', width: '100%' }}>
@@ -8,9 +8,13 @@ function ClockPresenter() {
       </div>
 
       <section style={{ height: 300, backgroundColor: '#ccc', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <h3>HH:MM:SS</h3>
+        <h3>{time}</h3>
       </section>
-    </div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <button style={{ flex: 1, height: 30 }} onClick={onClickAlarm(60000)}>1분</button>
+        <button style={{ flex: 1, height: 30 }} onClick={onClickAlarm(10000)}>10초</button>
+      </div>
+      </div>
   );
 }
 
